14897ba5736f6b66d35148c419fa798a6b9ca8fa
Sean Norton <Norton.Sean.M@gmail.com>
2016-10-03 20:14:07 -0500

    pulling jonbranch

--- popup.js
+++ popup.js
@@ -1,12 +1,22 @@
 console.log('found me');
 
 function click(e) {
-  chrome.tabs.executeScript(null);
+  chrome.tabs.executeScript(null,{code:"console.log('DUUUUUDE')"});
   window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  $('form').on('click', function(e){
-    console.log(e.target)
-  })
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
 });
+// chrome.browserAction.onClicked.addListener(function(tab){
+//   console.log('clicked!');
+//   chrome.tabs.executeScript(null, {file: "popup.js"});
+// });
+
+//check this out... link below
+// https://developer.chrome.com/extensions/browserAction#event-onClicked
+//
+// chrome.browserAction.onClicked.addListener(function callback)
